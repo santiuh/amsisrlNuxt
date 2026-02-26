1ac42f8314c5fc8b6d53f275818828fff4219682
Sanntiuh <sanntiagogirolami@gmail.com>
2026-02-25 21:26:56 -0300

    feat: install @nuxt/ui v2 and update supabase/nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,11 @@
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/supabase', '@nuxtjs/tailwindcss', '@nuxthub/core'],
-
-  tailwindcss: {
-    cssPath: '~/assets/css/tailwind.postcss',
-  },
+  modules: ['@nuxtjs/supabase', '@nuxt/ui', '@nuxthub/core'],
 
   supabase: {
     redirectOptions: {
-      login: '*',
-      callback: '*',
+      login: '/login',
+      callback: '/confirm',
+      exclude: [],
     },
   },
 
@@ -20,16 +17,13 @@ export default defineNuxtConfig({
     layoutTransition: { name: 'layout', mode: 'out-in' },
     pageTransition: { name: 'page', mode: 'out-in' },
     head: {
-      charset: 'utf-16',
-      viewport: 'width=500, initial-scale=1',
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-        { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
-        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Roboto+Mono&display=swap' },
       ],
     },
   },
 
   compatibilityDate: '2025-02-19',
-});
+})
